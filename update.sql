dae0c3822e443914026ec558414511db6d9cf8f4
Özge Dinçer <dncerozge@gmail.com>
2022-02-08 16:05:32 +0300

    Update update.sql

--- update.sql
+++ update.sql
@@ -8,3 +8,9 @@ UPDATE account_job
 SET hire_date = account.created_on
 FROM account
 WHERE account_job.user_id = account.user_id
+
+
+#3
+UPDATE account
+SET last_login = CURRENT_TIMESTAMP
+RETURNING email,created_on,last_login

6abecba300afbc0c9984c90e8ba8b42644337a64
Özge Dinçer <dncerozge@gmail.com>
2022-02-08 17:06:11 +0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -16,3 +16,8 @@ VALUES ('President')
 #4
 INSERT INTO account_job(user_id,job_id,hire_date)
 VALUES (2,1,CURRENT_TIMESTAMP)
+
+
+#5
+INSERT INTO employees(first_name,last_name,birthdate,hire_date,salary)
+VALUES ('Jose', 'Portilla', '1990-11-03', '2010-01-01', 100)

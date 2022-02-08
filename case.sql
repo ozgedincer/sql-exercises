172b2e9cb075ffe9ff2bae1545cf943f020aa408
Özge Dinçer <dncerozge@gmail.com>
2022-02-08 22:13:54 +0300

    Update case.sql

--- case.sql
+++ case.sql
@@ -6,3 +6,26 @@ CASE
 	ELSE 'Normal'
 END AS customer_class
 FROM customer
+
+
+#2
+SELECT 
+SUM(
+CASE rating
+	WHEN 'R' THEN 1 ELSE 0
+END 
+)
+AS r,
+SUM(
+CASE rating
+	WHEN 'PG' THEN 1 ELSE 0
+END 
+)
+AS pg,
+SUM(
+CASE rating
+	WHEN 'PG-13' THEN 1 ELSE 0
+END 
+)
+AS pg13
+FROM film
